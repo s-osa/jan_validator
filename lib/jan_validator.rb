21b0377180e07b4162c4f27b05448def5df0ece0
tetsuya <tetsuya@users.noreply.github.com>
2016-03-10 22:51:09 +0900

    Add I18n

--- lib/jan_validator.rb
+++ lib/jan_validator.rb
@@ -1,9 +1,11 @@
+require "active_support/i18n"
 require "jan_validator/version"
+require "jan_validator/engine" if defined?(Rails)
 require "jan"
 
 class JanValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, "is invalid length") unless Jan::Validator.validate_size(value)
-    record.errors.add(attribute, "is invalid check-digit") unless Jan::Validator.validate_check_digit(value)
+    record.errors.add(attribute, :invalid_length) unless Jan::Validator.validate_size(value)
+    record.errors.add(attribute, :invalid_check_digit) unless Jan::Validator.validate_check_digit(value)
   end
 end
